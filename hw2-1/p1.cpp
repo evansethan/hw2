2872d53819bdba8dbb6a8cba5c64a8c2eef2b60c
Ethan Evans <ethanevans.j@gmail.com>
2026-01-16 22:35:11 -0600

    p1

--- hw2-1/p1.cpp
+++ hw2-1/p1.cpp
@@ -9,3 +9,41 @@ of the corresponding element in the original vector and print the new vector
 (If you use ostream_iterator to print the new vector, you will likely get an
 extra comma at the end of the output. Don’t worry if that happens).
 */
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <format>
+#include <iterator>     // std::ostream_iterator
+using namespace std;
+
+
+vector<double> squareTransform(const vector<double>&v1) {
+
+    vector<double> v2(v1.size());
+
+    transform(v1.begin(), v1.end(), v2.begin(),
+              [](double a) {return a * a;});
+
+    return v2;
+}
+
+
+int main(){
+
+    vector<double> v1 = {1.23, 4.56, 7.89, 10.11};
+
+    vector<double> v2 = squareTransform(v1);
+
+
+    // for (double element : v2) {
+    //     cout << element << " ";
+    // }
+    // cout << endl;
+    std::ostream_iterator<double> out_it (std::cout,", ");
+    std::copy ( v2.begin(), v2.end(), out_it );
+    std::cout << std::endl;
+    return 0;
+
+}
